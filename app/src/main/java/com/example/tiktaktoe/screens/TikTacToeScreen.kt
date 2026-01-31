f5640662b4baa6262b3e7c138f9d63dc371b6074
IrfanShariff <irfanshariffs.basha@expleogroup.com>
2026-01-31 07:27:28 +0530

    This Commit has 9 square cells with text aligned center.

--- app/src/main/java/com/example/tiktaktoe/screens/TikTacToeScreen.kt
+++ app/src/main/java/com/example/tiktaktoe/screens/TikTacToeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.tiktaktoe.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,8 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TicTacToeScreen(modifier: Modifier = Modifier) {
@@ -29,14 +33,14 @@ fun TicTacToeScreen(modifier: Modifier = Modifier) {
             text = "Tic Tac Toe",
             style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 24.dp),
+            modifier = Modifier.padding(bottom = 20.dp),
         )
 
         Text(
             text = "Player X Start First",
             style = MaterialTheme.typography.headlineLarge,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 24.dp),
+            fontWeight = FontWeight.ExtraLight,
+            modifier = Modifier.padding(bottom = 18.dp),
         )
 
         GameBoard()
@@ -45,15 +49,32 @@ fun TicTacToeScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun GameBoard() {
-    Column(modifier = Modifier
-        .size(250.dp)
-        .border(2.dp, MaterialTheme.colorScheme.primary)) {
-        repeat(3){
+    Column(
+        modifier = Modifier
+            .size(250.dp)
+            .border(2.dp, MaterialTheme.colorScheme.primary)
+    ) {
+        repeat(3) {
             Row(modifier = Modifier.weight(1f)) {
-                repeat(3){
-                    Text(text = "O", modifier = Modifier.weight(1f))
+                repeat(3) {
+                    SquareCellUI(
+                        modifier = Modifier
+                            .weight(1f))
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SquareCellUI(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .border(1.dp, MaterialTheme.colorScheme.primary)
+            .background(color = Color.Transparent),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = " X ", fontWeight = FontWeight.Bold, fontSize = 36.sp, color = Color.Black)
     }
 }
